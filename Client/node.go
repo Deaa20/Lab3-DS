9efa9aaba7ad0a64e642aae9a49c76d28697ae55
Deaa20 <deaa20_20@hotmail.com>
2023-12-05 04:52:14 -0800

    rpc is working now

--- Client/node.go
+++ Client/node.go
@@ -48,12 +48,14 @@ func NewChord() {
 
 func JoinChord() {
 	Node.Predecessor = ""
-	args := FindSuccessorArgs{}
+	args := FindSuccessorArgs{String: "join"}
 	reply := FindSuccessorReply{}
 
-	ok := callNode("NodeClient.SendTest", args, reply, Node.JoinAddress, Node.JoinPort)
+	ok := callNode("NodeClient.SendTest", &args, &reply, Node.JoinAddress, Node.JoinPort)
+
+	//ok := callNode("NodeClient.SendTest", &args, &reply, Node.JoinAddress, Node.JoinPort)
 	if ok {
-		Node.ReciveTest(args, reply)
+		Node.ReciveTest(&args, &reply)
 
 	} else {
 		fmt.Print("here i am")
@@ -63,6 +65,7 @@ func JoinChord() {
 
 func callNode(rpcname string, args interface{}, reply interface{}, address string, port int) bool {
 	//Check if Sprint works
+
 	addressPort := address + ":" + fmt.Sprint(port)
 	c, err := rpc.DialHTTP("tcp", addressPort)
 
@@ -73,7 +76,6 @@ func callNode(rpcname string, args interface{}, reply interface{}, address strin
 
 	err = c.Call(rpcname, args, reply)
 	if err == nil {
-
 		return true
 	}
 
@@ -82,16 +84,15 @@ func callNode(rpcname string, args interface{}, reply interface{}, address strin
 }
 
 func (n *NodeClient) Server(port int) {
-
 	rpc.Register(n)
 	rpc.HandleHTTP()
 	portString := ":" + fmt.Sprint(port)
+
 	fmt.Print(portString + "...\n")
 	l, e := net.Listen("tcp", portString)
 	if e != nil {
 		log.Fatal("listen error:", e)
 	}
-
 	http.Serve(l, nil)
 
 }
@@ -100,7 +101,6 @@ func (n *NodeClient) Done() bool {
 	n.Lock.Lock()
 	defer n.Lock.Unlock()
 	ret := false
-
 	if n.Status == 0 {
 		ret = true
 	}
@@ -108,11 +108,15 @@ func (n *NodeClient) Done() bool {
 }
 
 func (n *NodeClient) SendTest(args *FindSuccessorArgs, reply *FindSuccessorReply) error {
-	args.String = "it works mf"
+	fmt.Print("checking if im here or not")
+
+	reply.String = "it works mf"
+	fmt.Print("it works mf-------------------------")
 	return nil
 }
-func (n *NodeClient) ReciveTest(args FindSuccessorArgs, reply FindSuccessorReply) error {
+func (n *NodeClient) ReciveTest(args *FindSuccessorArgs, reply *FindSuccessorReply) error {
 	fmt.Print(reply.String)
+	fmt.Print("it works mf-------------------------")
 	return nil
 }
 
